49c91d39504fdd572180719e303e68bbf4467e97
carboboop <carboboop@gmail.com>
2017-10-24 11:43:07 +0800

    Create Test1.java

---
+++ src/test/java/Test1.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Test1 {
+  @Test
+  public void evaluatesExpression() {
+    System.out.println("xxxxxxxxxxx");
+  }
+}
