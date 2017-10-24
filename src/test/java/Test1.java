a0264f75cf2efd6652603b2653f6a3d56048b329
carboboop <carboboop@gmail.com>
2017-10-24 14:27:45 +0800

    Update Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -5,6 +5,6 @@ public class Test1 {
   @Test
   public void evaluatesExpression() {
     System.out.println("xxxxxxxxxxxyyyyyyyyy");
-    org.junit.Assert.assertEquals("1","2");
+    org.junit.Assert.assertEquals("1","1");
   }
 }
